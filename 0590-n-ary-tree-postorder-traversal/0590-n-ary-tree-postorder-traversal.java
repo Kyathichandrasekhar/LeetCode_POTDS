80952f7e9e3b4e7c13ca4a3a0d3d02b41bec88f9
GANJALA KHYATHI CHANDRA SEKHAR <kyathiganjala@gmail.com>
2026-07-27 23:23:22 +0530

    Time: 0 ms (100%), Space: 46.6 MB (30.95%) - LeetHub

---
+++ 0590-n-ary-tree-postorder-traversal/0590-n-ary-tree-postorder-traversal.java
@@ -0,0 +1,38 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+}
+*/
+
+class Solution {
+    public void post(Node root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+
+        for (Node child : root.children) {
+            post(child, list);
+        }
+
+        list.add(root.val);
+    }
+
+    public List<Integer> postorder(Node root) {
+        List<Integer> list = new ArrayList<>();
+        post(root, list);
+        return list;
+    }
+}
