3137d66a1f0008be6b7d8deaa92d271c174afa48
GANJALA KHYATHI CHANDRA SEKHAR <kyathiganjala@gmail.com>
2026-08-23 21:16:44 +0530

    Time: 4 ms (74.65%), Space: 45.3 MB (68.09%) - LeetHub

---
+++ 4030-check-ascii-palindromic/4030-check-ascii-palindromic.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isPalindromic(String s) {
+        StringBuilder binary = new StringBuilder();
+
+        for(char ch : s.toCharArray()){
+            int ascii = (int)ch;
+
+            String bits = Integer.toBinaryString(ascii);
+
+            while(bits.length()<8){
+                bits = "0" + bits;
+            }
+            binary.append(bits);
+        }
+
+        int left = 0;
+        int right = binary.length()-1;
+
+        while(left<right){
+            if(binary.charAt(left)!= binary.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
