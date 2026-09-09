26c2117d84bcdfac52b1d952d9be5fca381db016
GANJALA KHYATHI CHANDRA SEKHAR <kyathiganjala@gmail.com>
2026-09-09 15:25:59 +0530

    Time: 8 ms (43.68%), Space: 47.2 MB (87.35%) - LeetHub

---
+++ 2428-maximum-sum-of-an-hourglass/2428-maximum-sum-of-an-hourglass.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxSum(int[][] mat) {
+        int n=mat.length;
+        int m=mat[0].length;
+        if(n<3 || m<3) return -1;
+        int max=Integer.MIN_VALUE;
+        int sum=0;
+        for(int i=0;i<n-2;i++){
+            for(int j=0;j<m-2;j++){
+                sum=mat[i][j]+mat[i][j+1]+mat[i][j+2]+mat[i+1][j+1]+mat[i+2][j]+mat[i+2][j+1]+mat[i+2][j+2];
+                max=Math.max(sum,max);
+            }
+        }
+        return max;
+
+    }
+}
